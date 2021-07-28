3805a9c9948f6f24649ea0f18cbd25a73c214400
Chris Dusyk <chris.dusyk@gmail.com>
2021-07-28 14:43:12 -0600

    Domain handlers and types

---
+++ src/TestingGiraffe.Domain/Domain.fs
@@ -0,0 +1,13 @@
+﻿module Domain
+
+let getAllGames (getAllgamesFromPersistence: unit -> Result<TestingGiraffe.Domain.Types.Game list, string>) =
+    let allGames = getAllgamesFromPersistence
+    allGames
+
+let getGameById (getGameByIdFromPersistence: (string -> Result<TestingGiraffe.Domain.Types.Game, string>)) (id: string) =
+    let game = getGameByIdFromPersistence id
+    game
+
+let insertNewGame (insertNewGameIntoPersistence: TestingGiraffe.Domain.Types.Game -> Result<unit, string>) (game: TestingGiraffe.Domain.Types.Game): Result<unit, string> =
+    let result = insertNewGameIntoPersistence game
+    result
---
+++ src/TestingGiraffe.Domain/Types.fs
@@ -0,0 +1,25 @@
+﻿namespace TestingGiraffe.Domain
+
+module Types =
+    open System
+
+    type Game = {
+        Id: Guid
+        Developer: string
+        Name: string
+        HasMultiplayer: bool
+    }
+    module Game =
+        let create (id: string) developer name hasMultiplayer : Result<Game, string> =
+            let success, parsedId = Guid.TryParse id
+            if not success then
+                id |> sprintf "Error parsing the Id: %s" |> Result.Error
+            elif String.IsNullOrWhiteSpace(developer) then
+                "Empty or invalid developer" |> Result.Error
+            elif String.IsNullOrWhiteSpace(name) then
+                "Empty or invalid name" |> Result.Error
+            else
+                { Id = parsedId
+                  Developer = developer
+                  Name = name
+                  HasMultiplayer = hasMultiplayer } |> Result.Ok
