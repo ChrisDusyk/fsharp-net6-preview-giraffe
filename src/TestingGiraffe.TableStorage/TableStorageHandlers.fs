f2fdef7067eb4e90ea769391351fa9ae55a8d4da
Chris Dusyk <chris.dusyk@gmail.com>
2021-07-28 14:43:26 -0600

    Table storage handlers (with results)

---
+++ src/TestingGiraffe.TableStorage/TableStorageHandlers.fs
@@ -0,0 +1,51 @@
+﻿namespace TestingGiraffe.TableStorage
+
+module TableStorageHandlers =
+    open FSharp.Azure.Storage.Table
+    open Microsoft.Azure.Cosmos.Table
+    open Types
+    open TestingGiraffe
+
+    let [<Literal>] private gamesTableName = "Games"
+
+    let private account = CloudStorageAccount.Parse "UseDevelopmentStorage=true"
+    let private tableClient = account.CreateCloudTableClient()
+
+    let private fromGameTable (query: EntityQuery<'a>) = fromTable tableClient gamesTableName query
+    let private inGameTable game = inTable tableClient gamesTableName game
+
+    let private toDomainGame (tableStorageGame: Game) =
+        Domain.Types.Game.create tableStorageGame.Id tableStorageGame.Developer tableStorageGame.Name tableStorageGame.HasMultiplayer
+    let private toTableStorageGame (domainGame: TestingGiraffe.Domain.Types.Game) : Game =
+        { Id = domainGame.Id.ToString()
+          Developer = domainGame.Developer
+          Name = domainGame.Name
+          HasMultiplayer = domainGame.HasMultiplayer }
+
+    let getAllGamesFromTableStorage =
+        fun _ ->
+            try
+                let games = Query.all<Game> |> fromGameTable |> Seq.map fst
+                games |> Result.traverseResult toDomainGame
+            with
+                | :? System.Exception as ex -> ex.ToString() |> sprintf "Error getting all games: %s" |> Result.Error
+
+    let getGameByIdFromTableStorage (id: string) =
+        try
+            let game =
+                Query.all<Game>
+                |> Query.where <@ fun g s -> s.RowKey = id @>
+                |> fromGameTable
+                |> Seq.map fst
+                |> Seq.head
+                |> toDomainGame
+            game
+        with
+            | :? System.Exception as ex -> ex.ToString() |> sprintf "Error getting game: %s" |> Result.Error
+
+    let insertGameIntoTableStorage (game: TestingGiraffe.Domain.Types.Game) =
+        let result = game |> toTableStorageGame |> Insert |> inGameTable
+        if result.HttpStatusCode >= 200 && result.HttpStatusCode < 300 then
+            Result.Ok ()
+        else
+            result.ToString() |> sprintf "Error saving game: %s" |> Result.Error
---
+++ src/TestingGiraffe.TableStorage/Types.fs
@@ -0,0 +1,24 @@
+﻿namespace TestingGiraffe.TableStorage
+
+module Types =
+    open FSharp.Azure.Storage.Table
+
+    type Game = {
+        [<PartitionKey>] Developer: string
+        [<RowKey>] Id: string
+        Name: string
+        HasMultiplayer: bool }
+
+module Result =
+    let traverseResult f list =
+        let (>>=) x f = Result.bind f x
+        let retn = Result.Ok
+
+        let cons head tail = head :: tail
+
+        let initState = retn []
+        let folder head tail =
+            f head >>= (fun h ->
+                tail >>= (fun t ->
+                    retn (cons h t) ))
+        Seq.foldBack folder list initState
