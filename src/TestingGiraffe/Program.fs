a95e51856f965868513ba5a8c88d9d0a0fbe5e36
Chris Dusyk <chris.dusyk@gmail.com>
2021-07-29 09:54:47 -0600

    Refactor api types into separate file

--- src/TestingGiraffe/Program.fs
+++ src/TestingGiraffe/Program.fs
@@ -5,35 +5,7 @@ open Microsoft.AspNetCore.Http
 open Giraffe
 open Giraffe.EndpointRouting
 open FSharp.Control.Tasks.V2.ContextInsensitive
-
-type GameResource = {
-    Developer: string
-    Id: System.Guid
-    Name: string
-    HasMultiplayer: bool }
-
-let toGameResource (game: Domain.Types.Game): GameResource =
-    { Developer = game.Developer
-      Id = game.Id
-      Name = game.Name
-      HasMultiplayer = game.HasMultiplayer }
-
-let toDomainGame (game: GameResource) =
-    let stringGameId = game.Id.ToString()
-    Domain.Types.Game.create stringGameId game.Developer game.Name game.HasMultiplayer
-
-type ErrorResponse = {
-    Message: string
-    StackTrace: string
-}
-
-let toErrorResponse (error: Domain.Types.ErrorType): ErrorResponse =
-    let stackTrace =
-        match error.InnerException with
-        | Some e -> e.StackTrace
-        | None _ -> ""
-    { Message = error.Message
-      StackTrace = stackTrace }
+open Types
 
 let matchErrorResponse serviceError =
     match serviceError with
